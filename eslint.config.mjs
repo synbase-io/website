688679283665012974b8d687cf2b928b49187100
Syntarex <4340303+Syntarex@users.noreply.github.com>
2024-05-03 16:17:37 +0200

    Refactor

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,4 +2,11 @@ import js from "@eslint/js";
 import prettier from "eslint-config-prettier";
 import ts from "typescript-eslint";
 
-export default [...ts.config(js.configs.recommended, ...ts.configs.recommended), prettier];
+const files = ["src/*_/_.{js,ts,jsx,tsx}"];
+
+export default [
+    ...ts.config(js.configs.recommended, ...ts.configs.recommended, {
+        files,
+    }),
+    { ...prettier, files },
+];
--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        /* eslint-disable no-undef */
         WEB_URL: process.env.WEB_URL,
         DISCORD_URL: process.env.DISCORD_URL,
-        /* eslint-enable no-undef */
+    },
+    eslint: {
+        ignoreDuringBuilds: true,
     },
 };
 
