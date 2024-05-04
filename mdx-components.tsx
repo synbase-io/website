23b9a2da63f728ac9904cefb853b8007f223c1f8
Syntarex <4340303+Syntarex@users.noreply.github.com>
2024-05-04 03:30:15 +0200

    Map markdown typography

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,3 +1,4 @@
+import { H1, H2, H3, H4, P, Quote } from "@/component/ui/typography";
 import type { MDXComponents } from "mdx/types";
 
 /**
@@ -7,6 +8,13 @@ import type { MDXComponents } from "mdx/types";
  */
 export const useMDXComponents = (components: MDXComponents): MDXComponents => {
     return {
+        h1: ({ children }) => <H1>{children}</H1>,
+        h2: ({ children }) => <H2>{children}</H2>,
+        h3: ({ children }) => <H3>{children}</H3>,
+        h4: ({ children }) => <H4>{children}</H4>,
+        p: ({ children }) => <P>{children}</P>,
+        blockquote: ({ children }) => <Quote>{children}</Quote>,
+        // TODO: Add code
         ...components,
     };
 };
