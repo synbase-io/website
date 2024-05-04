a8a432f80d4b0f7283dbbf74dff9cd321d2fbd17
Syntarex <4340303+Syntarex@users.noreply.github.com>
2024-05-04 03:02:45 +0200

    Configure next to use mdx

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,20 @@
+import createMDX from "@next/mdx";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
         WEB_URL: process.env.WEB_URL,
         DISCORD_URL: process.env.DISCORD_URL,
     },
+    pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"], // Includes markdown files
     eslint: {
         ignoreDuringBuilds: true,
     },
 };
 
-export default nextConfig;
+const withMDX = createMDX({
+    // Add markdown plugins here, as desired
+});
+
+// Merge MDX config with Next.js config
+export default withMDX(nextConfig);
